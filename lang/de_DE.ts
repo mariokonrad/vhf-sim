ef9ad5ad68c245395f6370cb6fb854f8dc67818c
Mario Konrad <mario.konrad@gmx.net>
2016-01-11 17:35:44 +0100

    Added support for internationalization

---
+++ lang/de_DE.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>radiosim::About</name>
+    <message>
+        <location filename="../src/action/About.cpp" line="8"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../src/action/About.cpp" line="9"/>
+        <source>Radio-Sim</source>
+        <translation>Radio-Sim</translation>
+    </message>
+</context>
+<context>
+    <name>radiosim::AboutQt</name>
+    <message>
+        <location filename="../src/action/AboutQt.cpp" line="8"/>
+        <source>AboutQt</source>
+        <translation>Über Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/action/AboutQt.cpp" line="9"/>
+        <source>Shows the AboutQt dialog</source>
+        <translation>Zeigt Informationen über Qt</translation>
+    </message>
+</context>
+<context>
+    <name>radiosim::ExitApplication</name>
+    <message>
+        <location filename="../src/action/ExitApplication.cpp" line="8"/>
+        <source>E&amp;xit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../src/action/ExitApplication.cpp" line="9"/>
+        <source>Closes the application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>radiosim::Fullscreen</name>
+    <message>
+        <location filename="../src/action/Fullscreen.cpp" line="10"/>
+        <source>Fullscreen</source>
+        <translation>Vollbild</translation>
+    </message>
+    <message>
+        <location filename="../src/action/Fullscreen.cpp" line="11"/>
+        <source>Shows the window in fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>radiosim::MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="39"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="42"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="45"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="53"/>
+        <source>Radio-Sim</source>
+        <translation type="unfinished">Radio-Sim</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="53"/>
+        <source>
+
+Version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="54"/>
+        <source>
+
+License: GPL</source>
+        <translation>
+
+Lizenz: GPL</translation>
+    </message>
+</context>
+</TS>
--- src/radio.cpp
+++ src/radio.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QDebug>
 #include "MainWindow.hpp"
 #include "version.hpp"
 
@@ -6,7 +8,12 @@ int main(int argc, char ** argv)
 {
 	QApplication app(argc, argv);
 	app.setApplicationName("Radio-Sim");
-	app.setApplicationVersion(radiosim::version);
+	app.setApplicationVersion(radiosim::project_version);
+
+	QTranslator translator;
+	translator.load(
+		"de_DE", app.applicationDirPath() + "/../share/" + radiosim::project_name + "/lang");
+	app.installTranslator(&translator);
 
 	radiosim::MainWindow win;
 	win.show();
