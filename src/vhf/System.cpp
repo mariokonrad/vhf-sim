5cd5805684c3a6ba0c7a3b1e937726c25bf3f414
Mario Konrad <mario.konrad@gmx.net>
2016-01-14 17:26:10 +0100

    buf fix: System load/save swapped, micro-cleanup

--- src/vhf/System.cpp
+++ src/vhf/System.cpp
@@ -48,47 +48,7 @@ bool System::load()
 	QSettings * config = sys.config;
 	if (!config)
 		return false;
-	config->setValue("global/type", sys.global.type.c_str());
-	config->setValue("global/lang", sys.global.lang.c_str());
-	config->setValue("global/exam_mode", sys.global.exam_mode);
-	config->setValue("com/host", sys.com.host.c_str());
-	config->setValue("com/port", sys.com.port);
-	config->setValue("gps/lat", sys.gps.lat.str().c_str());
-	config->setValue("gps/lon", sys.gps.lon.str().c_str());
-	config->setValue("gps/time", sys.gps.time.str().c_str());
-	config->setValue("gps/on_top", sys.gps.on_top);
-	config->setValue("vhf/mmsi", sys.vhf.mmsi.str().c_str());
-	config->setValue("vhf/group", sys.vhf.group.str().c_str());
-	config->setValue("vhf/time", sys.vhf.time.str().c_str());
-	config->setValue("vhf/lat", sys.vhf.lat.str().c_str());
-	config->setValue("vhf/lon", sys.vhf.lon.str().c_str());
-	config->setValue("cc/mmsi", sys.cc.mmsi.str().c_str());
-	config->setValue("cc/group", sys.cc.group.str().c_str());
-	config->setValue("cc/lat", sys.cc.lat.str().c_str());
-	config->setValue("cc/lon", sys.cc.lon.str().c_str());
-	config->setValue("cc/time", sys.cc.time.str().c_str());
-	config->setValue("cc/all_msg", sys.cc.all_msg);
-	config->setValue("cc/on_top", sys.cc.on_top);
-	config->beginWriteArray("dir");
-	for (auto i = 0u; i < sys.dir.size(); ++i) {
-		config->setArrayIndex(i);
-		config->setValue("name", sys.dir[i].name.c_str());
-		config->setValue("mmsi", sys.dir[i].mmsi.c_str());
-	}
-	config->endArray();
-	config->sync();
-	return true;
-}
 
-bool System::save()
-{
-	System & sys = inst();
-	if (!sys.mod)
-		return true;
-	QSettings * config = sys.config;
-	if (!config)
-		return false;
-	config->sync();
 	sys.global.type = config->value("global/type").toString().toStdString();
 	sys.global.lang = config->value("global/lang").toString().toStdString();
 	sys.global.exam_mode = config->value("global/exam_mode").toBool();
@@ -118,8 +78,288 @@ bool System::save()
 		sys.dir.emplace_back(name, mmsi);
 	}
 	config->endArray();
+
 	config->sync();
+	return true;
+}
+
+bool System::save()
+{
+	System & sys = inst();
+	if (!sys.mod)
+		return true;
+	QSettings * config = sys.config;
+	if (!config)
+		return false;
+	config->sync();
+
+	config->setValue("global/type", sys.global.type.c_str());
+	config->setValue("global/lang", sys.global.lang.c_str());
+	config->setValue("global/exam_mode", sys.global.exam_mode);
+	config->setValue("com/host", sys.com.host.c_str());
+	config->setValue("com/port", sys.com.port);
+	config->setValue("gps/lat", sys.gps.lat.str().c_str());
+	config->setValue("gps/lon", sys.gps.lon.str().c_str());
+	config->setValue("gps/time", sys.gps.time.str().c_str());
+	config->setValue("gps/on_top", sys.gps.on_top);
+	config->setValue("vhf/mmsi", sys.vhf.mmsi.str().c_str());
+	config->setValue("vhf/group", sys.vhf.group.str().c_str());
+	config->setValue("vhf/time", sys.vhf.time.str().c_str());
+	config->setValue("vhf/lat", sys.vhf.lat.str().c_str());
+	config->setValue("vhf/lon", sys.vhf.lon.str().c_str());
+	config->setValue("cc/mmsi", sys.cc.mmsi.str().c_str());
+	config->setValue("cc/group", sys.cc.group.str().c_str());
+	config->setValue("cc/lat", sys.cc.lat.str().c_str());
+	config->setValue("cc/lon", sys.cc.lon.str().c_str());
+	config->setValue("cc/time", sys.cc.time.str().c_str());
+	config->setValue("cc/all_msg", sys.cc.all_msg);
+	config->setValue("cc/on_top", sys.cc.on_top);
+	config->beginWriteArray("dir");
+	for (auto i = 0u; i < sys.dir.size(); ++i) {
+		config->setArrayIndex(i);
+		config->setValue("name", sys.dir[i].name.c_str());
+		config->setValue("mmsi", sys.dir[i].mmsi.c_str());
+	}
+	config->endArray();
+
 	sys.mod = false;
 	return true;
 }
+
+bool System::modified() { return inst().mod; }
+
+bool System::vhf_lat_set() { return inst()._lat; }
+
+bool System::vhf_lon_set() { return inst()._lon; }
+
+bool System::vhf_time_set() { return inst()._time; }
+
+void System::vhf_clear_pos_time()
+{
+	System & sys = inst();
+	sys._time = false;
+	sys._lat = false;
+	sys._lon = false;
+}
+
+void System::sound_enable(bool flag) { inst().sound_status = flag; }
+
+bool System::sound_enabled() { return inst().sound_status; }
+
+bool System::sound_disabled() { return !inst().sound_status; }
+
+void System::lang(const std::string & lang)
+{
+	System & sys = inst();
+	if (sys.global.lang != lang) {
+		sys.global.lang = lang;
+		sys.mod = true;
+	}
+}
+
+std::string System::lang() { return inst().global.lang; }
+
+void System::exam_mode(bool f)
+{
+	System & sys = inst();
+	if (sys.global.exam_mode != f)
+		sys.mod = true;
+	sys.global.exam_mode = f;
+}
+
+bool System::exam_mode() { return inst().global.exam_mode; }
+
+std::string System::type() { return inst().global.type; }
+
+void System::com_host(const std::string & s)
+{
+	System & sys = inst();
+	if (sys.com.host != s)
+		sys.mod = true;
+	sys.com.host = s;
+}
+
+std::string System::com_host() { return inst().com.host; }
+
+void System::com_port(short p)
+{
+	System & sys = inst();
+	if (sys.com.port != p)
+		sys.mod = true;
+	sys.com.port = p;
+}
+
+short System::com_port() { return inst().com.port; }
+
+void System::gps_latitude(const engine::Latitude & l)
+{
+	System & sys = inst();
+	if (sys.gps.lat != l)
+		sys.mod = true;
+	sys.gps.lat = l;
+}
+
+engine::Latitude System::gps_latitude() { return inst().gps.lat; }
+
+void System::gps_longitude(const engine::Longitude & l)
+{
+	System & sys = inst();
+	if (sys.gps.lon != l)
+		sys.mod = true;
+	sys.gps.lon = l;
+}
+
+engine::Longitude System::gps_longitude() { return inst().gps.lon; }
+
+void System::gps_time(const engine::Date & t)
+{
+	System & sys = inst();
+	if (sys.gps.time != engine::Date(t))
+		sys.mod = true;
+	sys.gps.time = t;
+}
+
+engine::Date System::gps_time() { return inst().gps.time; }
+
+void System::gps_on_top(bool f)
+{
+	System & sys = inst();
+	if (sys.gps.on_top != f)
+		sys.mod = true;
+	sys.gps.on_top = f;
+}
+
+bool System::gps_on_top() { return inst().gps.on_top; }
+
+void System::dir_set(const engine::Directory & dir)
+{
+	System & sys = inst();
+	sys.dir = dir;
+	sys.mod = true;
+}
+
+engine::Directory System::dir_get() { return inst().dir; }
+
+void System::vhf_mmsi(const engine::MMSI & mmsi)
+{
+	System & sys = inst();
+	if (sys.vhf.mmsi != mmsi)
+		sys.mod = true;
+	sys.vhf.mmsi = mmsi;
+}
+
+engine::MMSI System::vhf_mmsi() { return inst().vhf.mmsi; }
+
+void System::vhf_group(const engine::MMSI & mmsi)
+{
+	System & sys = inst();
+	if (sys.vhf.group != mmsi)
+		sys.mod = true;
+	sys.vhf.group = mmsi;
+}
+
+engine::MMSI System::vhf_group() { return inst().vhf.group; }
+
+void System::vhf_latitude(const engine::Latitude & l)
+{
+	System & sys = inst();
+	if (sys.vhf.lat != l)
+		sys.mod = true;
+	sys._lat = true;
+	sys.vhf.lat = l;
+}
+
+engine::Latitude System::vhf_latitude() { return inst().vhf.lat; }
+
+void System::vhf_longitude(const engine::Longitude & l)
+{
+	System & sys = inst();
+	if (sys.vhf.lon != l)
+		sys.mod = true;
+	sys._lon = true;
+	sys.vhf.lon = l;
+}
+
+engine::Longitude System::vhf_longitude() { return inst().vhf.lon; }
+
+void System::vhf_time(const engine::Date & t)
+{
+	System & sys = inst();
+	if (sys.vhf.time != engine::Date(t))
+		sys.mod = true;
+	sys._time = true;
+	sys.vhf.time = t;
+}
+
+engine::Date System::vhf_time() { return inst().vhf.time; }
+
+void System::cc_mmsi(const engine::MMSI & mmsi)
+{
+	System & sys = inst();
+	if (sys.cc.mmsi != mmsi)
+		sys.mod = true;
+	sys.cc.mmsi = mmsi;
+}
+
+engine::MMSI System::cc_mmsi() { return inst().cc.mmsi; }
+
+void System::cc_group(const engine::MMSI & mmsi)
+{
+	System & sys = inst();
+	if (sys.cc.group != mmsi)
+		sys.mod = true;
+	sys.cc.group = mmsi;
+}
+
+engine::MMSI System::cc_group() { return inst().cc.group; }
+
+void System::cc_latitude(const engine::Latitude & l)
+{
+	System & sys = inst();
+	if (sys.cc.lat != l)
+		sys.mod = true;
+	sys.cc.lat = l;
+}
+
+engine::Latitude System::cc_latitude() { return inst().cc.lat; }
+
+void System::cc_longitude(const engine::Longitude & l)
+{
+	System & sys = inst();
+	if (sys.cc.lon != l)
+		sys.mod = true;
+	sys.cc.lon = l;
+}
+
+engine::Longitude System::cc_longitude() { return inst().cc.lon; }
+
+void System::cc_time(const engine::Date & t)
+{
+	System & sys = inst();
+	if (sys.cc.time != engine::Date(t))
+		sys.mod = true;
+	sys.cc.time = t;
+}
+
+engine::Date System::cc_time() { return inst().cc.time; }
+
+void System::cc_all_msg(bool flag)
+{
+	System & sys = inst();
+	if (sys.cc.all_msg != flag)
+		sys.mod = true;
+	sys.cc.all_msg = flag;
+}
+
+bool System::cc_all_msg() { return inst().cc.all_msg; }
+
+void System::cc_on_top(bool f)
+{
+	System & sys = inst();
+	if (sys.cc.on_top != f)
+		sys.mod = true;
+	sys.cc.on_top = f;
+}
+
+bool System::cc_on_top() { return inst().cc.on_top; }
 }
--- src/vhf/System.hpp
+++ src/vhf/System.hpp
@@ -76,232 +76,90 @@ public:
 	static bool load();
 	static bool save();
 
-	static bool modified() { return inst().mod; }
-	static bool vhf_lat_set() { return inst()._lat; }
-	static bool vhf_lon_set() { return inst()._lon; }
-	static bool vhf_time_set() { return inst()._time; }
-
-	static void vhf_clear_pos_time()
-	{
-		System & sys = inst();
-		sys._time = false;
-		sys._lat = false;
-		sys._lon = false;
-	}
+	static bool modified();
+	static bool vhf_lat_set();
+	static bool vhf_lon_set();
+	static bool vhf_time_set();
+	static void vhf_clear_pos_time();
 
 	// sound ------------------------------------------
 
-	static void sound_enable(bool flag) { inst().sound_status = flag; }
-	static bool sound_enabled() { return inst().sound_status; }
-	static bool sound_disabled() { return !inst().sound_status; }
+	static void sound_enable(bool flag);
+	static bool sound_enabled();
+	static bool sound_disabled();
 
 	// channels ---------------------------------------
 
 	// global -----------------------------------------
-	static void lang(const std::string & lang)
-	{
-		System & sys = inst();
-		if (sys.global.lang != lang) {
-			sys.global.lang = lang;
-			sys.mod = true;
-		}
-	}
+	static void lang(const std::string & lang);
+	static std::string lang();
 
-	static void exam_mode(bool f)
-	{
-		System & sys = inst();
-		if (sys.global.exam_mode != f)
-			sys.mod = true;
-		sys.global.exam_mode = f;
-	}
+	static void exam_mode(bool f);
+	static bool exam_mode();
 
-	static std::string lang() { return inst().global.lang; }
-
-	static std::string type() { return inst().global.type; }
-	static bool exam_mode() { return inst().global.exam_mode; }
+	static std::string type();
 
 	// communication ----------------------------------
-	static void com_host(const std::string & s)
-	{
-		System & sys = inst();
-		if (sys.com.host != s)
-			sys.mod = true;
-		sys.com.host = s;
-	}
+	static void com_host(const std::string & s);
+	static std::string com_host();
 
-	static void com_port(short p)
-	{
-		System & sys = inst();
-		if (sys.com.port != p)
-			sys.mod = true;
-		sys.com.port = p;
-	}
-
-	static std::string com_host() { return inst().com.host; }
-	static short com_port() { return inst().com.port; }
+	static void com_port(short p);
+	static short com_port();
 
 	// gps --------------------------------------------
-	static void gps_latitude(const engine::Latitude & l)
-	{
-		System & sys = inst();
-		if (sys.gps.lat != l)
-			sys.mod = true;
-		sys.gps.lat = l;
-	}
+	static void gps_latitude(const engine::Latitude & l);
+	static engine::Latitude gps_latitude();
 
-	static void gps_longitude(const engine::Longitude & l)
-	{
-		System & sys = inst();
-		if (sys.gps.lon != l)
-			sys.mod = true;
-		sys.gps.lon = l;
-	}
+	static void gps_longitude(const engine::Longitude & l);
+	static engine::Longitude gps_longitude();
 
-	static void gps_time(const engine::Date & t)
-	{
-		System & sys = inst();
-		if (sys.gps.time != engine::Date(t))
-			sys.mod = true;
-		sys.gps.time = t;
-	}
+	static void gps_time(const engine::Date & t);
+	static engine::Date gps_time();
 
-	static void gps_on_top(bool f)
-	{
-		System & sys = inst();
-		if (sys.gps.on_top != f)
-			sys.mod = true;
-		sys.gps.on_top = f;
-	}
-
-	static engine::Latitude gps_latitude() { return inst().gps.lat; }
-	static engine::Longitude gps_longitude() { return inst().gps.lon; }
-	static engine::Date gps_time() { return inst().gps.time; }
-	static bool gps_on_top() { return inst().gps.on_top; }
+	static void gps_on_top(bool f);
+	static bool gps_on_top();
 
 	// dir --------------------------------------------
-	static void dir_set(const engine::Directory & dir)
-	{
-		System & sys = inst();
-		sys.dir = dir;
-		sys.mod = true;
-	}
-
-	static engine::Directory dir_get() { return inst().dir; }
+	static void dir_set(const engine::Directory & dir);
+	static engine::Directory dir_get();
 
 	// vhf --------------------------------------------
-	static void vhf_mmsi(const engine::MMSI & mmsi)
-	{
-		System & sys = inst();
-		if (sys.vhf.mmsi != mmsi)
-			sys.mod = true;
-		sys.vhf.mmsi = mmsi;
-	}
+	static void vhf_mmsi(const engine::MMSI & mmsi);
+	static engine::MMSI vhf_mmsi();
 
-	static void vhf_group(const engine::MMSI & mmsi)
-	{
-		System & sys = inst();
-		if (sys.vhf.group != mmsi)
-			sys.mod = true;
-		sys.vhf.group = mmsi;
-	}
+	static void vhf_group(const engine::MMSI & mmsi);
+	static engine::MMSI vhf_group();
 
-	static void vhf_latitude(const engine::Latitude & l)
-	{
-		System & sys = inst();
-		if (sys.vhf.lat != l)
-			sys.mod = true;
-		sys._lat = true;
-		sys.vhf.lat = l;
-	}
+	static void vhf_latitude(const engine::Latitude & l);
+	static engine::Latitude vhf_latitude();
 
-	static void vhf_longitude(const engine::Longitude & l)
-	{
-		System & sys = inst();
-		if (sys.vhf.lon != l)
-			sys.mod = true;
-		sys._lon = true;
-		sys.vhf.lon = l;
-	}
+	static void vhf_longitude(const engine::Longitude & l);
+	static engine::Longitude vhf_longitude();
 
-	static void vhf_time(const engine::Date & t)
-	{
-		System & sys = inst();
-		if (sys.vhf.time != engine::Date(t))
-			sys.mod = true;
-		sys._time = true;
-		sys.vhf.time = t;
-	}
-
-	static engine::MMSI vhf_mmsi() { return inst().vhf.mmsi; }
-	static engine::MMSI vhf_group() { return inst().vhf.group; }
-	static engine::Latitude vhf_latitude() { return inst().vhf.lat; }
-	static engine::Longitude vhf_longitude() { return inst().vhf.lon; }
-	static engine::Date vhf_time() { return inst().vhf.time; }
+	static void vhf_time(const engine::Date & t);
+	static engine::Date vhf_time();
 
 	// control center ---------------------------------
-	static void cc_mmsi(const engine::MMSI & mmsi)
-	{
-		System & sys = inst();
-		if (sys.cc.mmsi != mmsi)
-			sys.mod = true;
-		sys.cc.mmsi = mmsi;
-	}
+	static void cc_mmsi(const engine::MMSI & mmsi);
+	static engine::MMSI cc_mmsi();
 
-	static void cc_group(const engine::MMSI & mmsi)
-	{
-		System & sys = inst();
-		if (sys.cc.group != mmsi)
-			sys.mod = true;
-		sys.cc.group = mmsi;
-	}
+	static void cc_group(const engine::MMSI & mmsi);
+	static engine::MMSI cc_group();
 
-	static void cc_latitude(const engine::Latitude & l)
-	{
-		System & sys = inst();
-		if (sys.cc.lat != l)
-			sys.mod = true;
-		sys.cc.lat = l;
-	}
+	static void cc_latitude(const engine::Latitude & l);
+	static engine::Latitude cc_latitude();
 
-	static void cc_longitude(const engine::Longitude & l)
-	{
-		System & sys = inst();
-		if (sys.cc.lon != l)
-			sys.mod = true;
-		sys.cc.lon = l;
-	}
+	static void cc_longitude(const engine::Longitude & l);
+	static engine::Longitude cc_longitude();
 
-	static void cc_time(const engine::Date & t)
-	{
-		System & sys = inst();
-		if (sys.cc.time != engine::Date(t))
-			sys.mod = true;
-		sys.cc.time = t;
-	}
+	static void cc_time(const engine::Date & t);
+	static engine::Date cc_time();
 
-	static void cc_all_msg(bool flag)
-	{
-		System & sys = inst();
-		if (sys.cc.all_msg != flag)
-			sys.mod = true;
-		sys.cc.all_msg = flag;
-	}
+	static void cc_all_msg(bool flag);
+	static bool cc_all_msg();
 
-	static void cc_on_top(bool f)
-	{
-		System & sys = inst();
-		if (sys.cc.on_top != f)
-			sys.mod = true;
-		sys.cc.on_top = f;
-	}
-
-	static engine::MMSI cc_mmsi() { return inst().cc.mmsi; }
-	static engine::MMSI cc_group() { return inst().cc.group; }
-	static engine::Latitude cc_latitude() { return inst().cc.lat; }
-	static engine::Longitude cc_longitude() { return inst().cc.lon; }
-	static engine::Date cc_time() { return inst().cc.time; }
-	static bool cc_all_msg() { return inst().cc.all_msg; }
-	static bool cc_on_top() { return inst().cc.on_top; }
+	static void cc_on_top(bool f);
+	static bool cc_on_top();
 };
 }
 
